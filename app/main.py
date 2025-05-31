13ca940c263bbb03394f7eb9d0e1328ef37fff86
Denis <godlike.kipish@gmail.com>
2025-05-31 20:57:32 +0300

    Maybe result!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    list_of_names = command.split()
+    if len(list_of_names) == 3 and list_of_names[0] == "cp":
+        file_to_copy = list_of_names[1]
+        copied_file = list_of_names[2]
+        if file_to_copy == copied_file:
+            return
+        if os.path.exists(file_to_copy):
+            with (open(file_to_copy, "r") as file1,
+                  open(copied_file, "w") as file2):
+                file2.write(file1.read())
